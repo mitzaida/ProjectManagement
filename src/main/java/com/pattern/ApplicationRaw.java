8e41c57b01c28af5659210b7a4777a680964386f
mitzaida <mitzyme.z@gmail.com>
2021-04-06 19:16:46 -0500

    Comments added

--- src/main/java/com/pattern/ApplicationRaw.java
+++ src/main/java/com/pattern/ApplicationRaw.java
@@ -15,33 +15,41 @@ public class ApplicationRaw {
         ApplicationRaw ar = new ApplicationRaw();
         int contador = 0;
 
+        // Obtener todos los productos.
         System.out.println("Productos en almacen:");
         for (String valor : DataUtil.getNombreProductos()) {
             System.out.println(valor);
 
+            // Si existe un producto que con tenga la letra B o C entonces almacenarlos en los detalles de la clase principal
             if (valor.contains("B") || valor.contains("C")) {
                 contador++;
                 ar.getDetalles().add(valor);
             }
         }
 
+        // Crear la factura e inicializar sus atributos
         Factura factura = new Factura();
         factura.setProductos(new ArrayList<>());
 
+        // Agregar los productos contenidos en el detalle de la clase principal
         System.out.println("Productos a ser agregados");
         for (String valor : ar.getDetalles()) {
             System.out.println(valor);
             factura.getProductos().add(valor);
         }
 
+        // Calcular el monto total de los productos agregados
         BigDecimal total = new BigDecimal(0);
         for (String item : factura.getProductos()) {
             BigDecimal precioProducto = DataUtil.getPrecioProducto(item);
             System.out.println("item: " + item + ", Precio: " + precioProducto.longValue());
             total = total.add(precioProducto);
         }
+
+        // asignar a la factura el monto total calculado
         factura.setTotal(total);
 
+        // Mostrar en consola la cantidad de items y el monto total de la factura
         System.out.println("Número de Items incluidos en la factura : " + contador);
         System.out.println("Total de la factura : " + factura.getTotal().floatValue());
 
