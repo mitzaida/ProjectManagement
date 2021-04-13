1cb943c9e5f973d13003f40f9d34eb27ca48b466
Mitzaida Medina <mitzyme.z@gmail.com>
2021-04-13 11:28:48 -0500

    areas e improved_empleado y la X

--- src/main/java/com/hacerLaX/ExamQuestionEvalArt.java
+++ src/main/java/com/hacerLaX/ExamQuestionEvalArt.java
@@ -3,9 +3,9 @@ package com.hacerLaX;
 public class ExamQuestionEvalArt {
 
     public static void main(String[] args) {
-        int n=5, fila, columna;
+        int n = 15, fila, columna;
 
-        String matrixDeX [][] = new String[n][n];
+        String matrixDeX[][] = new String[n][n+10];
 
         System.out.println("Tamaño de la matrixDeX: " + matrixDeX.length);
         int longitud = matrixDeX.length;
@@ -13,26 +13,58 @@ public class ExamQuestionEvalArt {
         System.out.println("MATRIX por cada posición bidimensional.");
 
         for (columna=0 ; columna < matrixDeX.length ; columna++ ) {
-
             for (fila=0 ; fila < matrixDeX.length; fila++) {
                 matrixDeX[columna] [fila]= "_";
             }
-
         }
+        flujoIncorrecto(n, matrixDeX);
+//      flujoCorrecto(n, matrixDeX);
+    }
 
-        for (columna = 0 ; columna < n ; columna++) {
-            for (fila = 0; fila < n ; fila++) {
-                //if ((columna==0 && fila==0) || (fila == n-1 && columna==n) || (columna == n && fila ==n) {
-                //if ((columna==0 && fila==0) || (fila == n-1 && columna==n) || (columna == n && fila ==n)) {
-                if ((columna==0 && fila==0) || (columna == n && fila ==n)) {
-                    matrixDeX[columna] [fila] = "X";
+    private static void flujoIncorrecto(int n, String[][] matrixDeX) {
+        int columna;
+        int fila;
+        int nn = n;
+        for (columna = 0; columna <  matrixDeX.length; columna++) {
+
+            for (fila = 0; fila <  matrixDeX[0].length; fila++) {
+
+                if (fila==nn-1) {
+                    matrixDeX[columna][fila] = "X";
+                    nn = nn - 1;
+
+                } else if (columna == fila) {
+                    matrixDeX[columna][fila] = "X";
+                } else {
+                    matrixDeX[columna][fila] = "_";
                 }
-                if (fila == n-1) {
-                    System.out.println("\n");
+                System.out.print(matrixDeX[columna][fila]);
+            }
+            System.out.println("");
+        }
+    }
+
+    public static void flujoCorrecto( int  n, String matrixDeX[][]) {
+        int nn = n;
+        for (int columna = 0 ; columna <  matrixDeX.length ; columna++) {
+
+            for (int fila = 0; fila <  matrixDeX[0].length ; fila++) {
+                if ((columna==fila) || (fila==nn-1)) {
+                    matrixDeX[columna] [fila] = "X";
+
+                }
+
+                if (fila==nn-1 )  {
+                    matrixDeX[columna] [fila] = "N";
+                    nn=nn-1;
                 }
 
                 System.out.print(matrixDeX[columna] [fila]);
             }
+
+            System.out.println("");
         }
+
     }
+
 }
